b3bcbc4c6046caf4fe628bbe3ee007f6f469653c
rampage36 <deliro.panov@icloud.com>
2023-02-04 08:50:49 +0300

    Basics

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Navbar from './components/Navbar/Navbar'
+import Header from './components/Header/Header'
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+    </div>);
+}
+
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import logo from "../Img/logo.png"
+
+const Header = () => {
+    return (<div className="Header">
+        <img src={ logo } alt={"logo"}/> 
+        
+    </div>);
+}
+
+export default Header;
