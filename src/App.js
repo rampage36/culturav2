c3d8a0604f97648ca792f3c675be8f35638eae9a
rampage36 <deliro.panov@icloud.com>
2023-02-04 09:09:45 +0300

    Basics 2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import Header from './components/Header/Header'
+import About from './components/About/About'
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Navbar />
+    <div className='app-wrapper'>
+      <Header className='header' />
+      <Navbar className='navbar' />
+      <About className='about' />
     </div>);
 }
 
