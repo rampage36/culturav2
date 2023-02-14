998395becc7f8a7600140d59a78a201d2a7abaca
rampage36 <deliro.panov@icloud.com>
2023-02-14 12:22:42 +0300

    v 7

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 import css from "./Photo.module.css"
-import Deliro from '../Img/Delirophoto.png'
+import Deliro from '../Img/Delirophoto1.png'
 
 export const Photo = () => {
     return (
         <div className={css.photo}>
-            <div className="Deliro"></div>
-            <img className={css.Deliro} src = {Deliro} alt = "Deliro_photo"/>    
+            <div className="Deliro">
+            <img className={css.Deliro} src = {Deliro} alt = "Deliro_photo"/>
+            <div className={css.DeliroText}>
+            Cras volutpat tempus felis,<br/> in iaculis <span>neque eleifend</span> quis.<br/>
+            Donec lobortis urna leo, a gravida elit varius a.
+            </div>
+            </div>
         </div>
     )
 }
